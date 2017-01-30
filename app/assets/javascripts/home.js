267ca94023706c7fdbdd1321549b4088cfd21c84
Owais Khan <owaiswiz@gmail.com>
2017-01-31 01:43:27 +0530

    Completed Templates for Light Device. Ongoing Work for Fans

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,23 @@
 $(document).ready(function(){
 
 	var doing=false;
-	$('#rangeText').text($('#rangeInput').val());
-  	// setup an event handler to set the text when the range value is dragged (see event for input) or changed (see event for change)
-  $('#rangeInput').on('input change', function () {
-  	$('#rangeText').text($(this).val());
+
+	function delayRequest(){
+		window.setTimeout(function(){
+			doing=false;
+		},2000);
+	}
+
+	function changeOpacity(ele){
+		$(ele).css("opacity",$(ele).siblings(".brightnessInput").val()/100.0 + 0.3)
+	}
+
+
+
+	//BrightnessHandler
+  $('.brightnessInput').on('input change', function () {
+  	changeOpacity($(this).siblings("i.light"));
+  	$(this).siblings('.brightnessText').text("Brightness: " + $(this).val() + "%");
   	if(!doing)
   	{
   		doing=true;
@@ -14,9 +27,26 @@ $(document).ready(function(){
   		return false;
 	});
 
-	function delayRequest(){
-		window.setTimeout(function(){
-			doing=false;
-		},2000);
-	}
+  //FanSpeedHandler
+  $(".speedInput").on('input change',function(){
+  	var duration = 4.0*0.5**($(this).val()-1) + 's';
+  	console.log(duration)
+  	$(this).siblings("i.fan").css("animation-duration",duration);
+  	$(this).siblings(".speedText").text("Speed: " + $(this).val());
+  });
+
+		$(".color-label").click(function(){
+		$(this).parents(".color-selector").find(".color-label").removeClass("active-color");
+		$(this).addClass("active-color")
+	});
+
+	$(".device i.fa-lightbulb-o").click(function(){
+		changeOpacity(this);
+		$(this).toggleClass("light-enabled light-disabled");
+		$(this).siblings(".brightnessInput").prop('disabled', function(i, v) { return !v; });
+	});
+
+	$(".device .color-label").click(function(){
+		$(this).parents(".device-controls").find("i.fa.fa-lightbulb-o").css("color",$(this).siblings("input").val());
+	});
 });
